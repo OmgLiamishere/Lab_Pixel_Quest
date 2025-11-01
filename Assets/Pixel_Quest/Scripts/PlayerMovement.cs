6e4ce8107c749d46549c464052c7647de1f1abef
OmgLiamishere <dragonmasterplayer1127@gmail.com>
2025-11-01 12:23:01 -0400

    Jumping moment Part1

--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
     public int speed = 5;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
+        sr = transform.GetComponentInChildren<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -17,5 +20,10 @@ public class PlayerMovement : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+        if (xInput > 0) {sr.flipX = true; }
+        else if (xInput < 0) { sr.flipX = false;}
+
+        
     }
 }
