b87f31fefd2b9c4edd58130c8e35b68d29ecfe35
OmgLiamishere <dragonmasterplayer1127@gmail.com>
2025-11-15 14:28:34 -0500

    Hands-on-hwk 2

--- Assets/Pixel_Quest/Scripts/PlayerUIController.cs
+++ Assets/Pixel_Quest/Scripts/PlayerUIController.cs
@@ -7,10 +7,12 @@ public class PlayerUIController : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
-        
+
     }
 
-    private void PrintGivenNumber(int number)
+    
+
+
     // Update is called once per frame
     void Update()
     {
